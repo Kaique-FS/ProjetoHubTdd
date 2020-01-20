f4805d138b489a8e135104d8628fc5c48110d4c1
Kaique <kaique.silva@rsinet.com.br>
2020-01-20 14:11:05 -0300

    Adição Classe ScreenShot

---
+++ src/main/java/br/com/rsinet/hub/projetotdd/utility/ScreenShot.java
@@ -0,0 +1,17 @@
+package br.com.rsinet.hub.projetotdd.utility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenShot extends Driver{
+
+	public static void ScreenShotCapture(WebDriver driver) throws IOException{
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(src, new File("C:\\Users\\kaique.silva\\eclipse-workspace\\projetotddteste\\screenshot\\" + System.currentTimeMillis() + ".png"));
+	}	
+}
