86938d539531fcb85ed32c03006d60ed7002ae97
Kaique <kaique.silva@rsinet.com.br>
2020-01-20 14:04:52 -0300

    Adicionando class Constant

---
+++ src/main/java/br/com/rsinet/hub/projetotdd/utility/Constant.java
@@ -0,0 +1,14 @@
+package br.com.rsinet.hub.projetotdd.utility;
+
+public class Constant {
+
+	public static final String URL = "http://www.advantageonlineshopping.com/#/";
+
+	public static final String Username = "Tritao";
+
+	public static final String Password = "Tridente1";
+
+	public static final String Product = "HP Z3200 WIRELESS MOUSE"; //"caneca"; 
+
+	public static final String Path_TestData = "C:\\Users\\kaique.silva\\Desktop\\aulas\\Register\\Register.xlsx";
+}
