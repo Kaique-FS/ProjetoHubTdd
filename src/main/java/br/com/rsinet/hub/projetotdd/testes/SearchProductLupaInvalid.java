2ca14e6a050a64657be806d8d1d38f92b86966dc
Kaique <kaique.silva@rsinet.com.br>
2020-01-21 14:58:09 -0300

    Adicionando cenario Pesquisar Lupa negativo

---
+++ src/main/java/br/com/rsinet/hub/projetotdd/testes/SearchProductLupaInvalid.java
@@ -0,0 +1,61 @@
+package br.com.rsinet.hub.projetotdd.testes;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import br.com.rsinet.hub.projetotdd.actions.Product;
+import br.com.rsinet.hub.projetotdd.actions.SearchLupa;
+import br.com.rsinet.hub.projetotdd.pageobjects.HomePage;
+import br.com.rsinet.hub.projetotdd.pageobjects.LogInPage;
+import br.com.rsinet.hub.projetotdd.utility.Constant;
+import br.com.rsinet.hub.projetotdd.utility.Driver;
+import br.com.rsinet.hub.projetotdd.utility.ScreenShot;
+
+public class SearchProductLupaInvalid {
+
+private static WebDriver driver;
+private static Logger Log = Logger.getLogger("Acao");
+
+	
+	@BeforeMethod
+	public void inicializa() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaique.silva\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(Constant.URL);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void Execute() throws Exception {
+		DOMConfigurator.configure("log4j.xml");
+
+		SearchLupa.btn_lupa(driver).click();
+		ScreenShot.ScreenShotCapture(driver);
+		Log.info("Clica na lupa + print");
+		
+		SearchLupa.txt_pesquisa(driver).sendKeys(Constant.ProductInvalid + Keys.ENTER);
+		SearchLupa.btn_closepopup(driver).click();
+		ScreenShot.ScreenShotCapture(driver);
+		Log.info("Escreve o produto + enter + print");
+		
+		Assert.assertEquals(Constant.ProductInvalid, "Biscoito");
+	}
+	
+	@AfterMethod
+	public void Finaliza() throws IOException {
+		ScreenShot.ScreenShotCapture(driver);
+		Driver.closeBrowser(driver);
+		Log.info("Tirou print e fechou navegador");
+	}
+}
